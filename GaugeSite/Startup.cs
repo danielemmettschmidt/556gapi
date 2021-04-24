5abd9ada3fa0f705f45ae1470ff6f4f0cb44253e
Daniel <81110392+prod-daniel-emmettschmidt@users.noreply.github.com>
2021-04-24 09:29:48 -0400

    Revert "Trying mysql options"
    This reverts commit 4a9b65c5924c1a604cc28d02bec6a616b3dbcb94.

--- GaugeSite/Startup.cs
+++ GaugeSite/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MySQL.Data.EntityFrameworkCore;
 
 namespace GaugeSite
 {
@@ -23,7 +22,7 @@ namespace GaugeSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<GaugeContext>(opt => opt.UseMySQL
+            services.AddDbContext<GaugeContext>(opt => opt.UseSqlServer
                 (Configuration.GetConnectionString("GaugeConnection")));
 
             services.AddControllersWithViews();
