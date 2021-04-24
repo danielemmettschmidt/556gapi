4a9b65c5924c1a604cc28d02bec6a616b3dbcb94
Daniel <81110392+prod-daniel-emmettschmidt@users.noreply.github.com>
2021-04-24 09:23:02 -0400

    Trying mysql options

--- GaugeSite/Startup.cs
+++ GaugeSite/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySQL.Data.EntityFrameworkCore;
 
 namespace GaugeSite
 {
@@ -22,7 +23,7 @@ namespace GaugeSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<GaugeContext>(opt => opt.UseSqlServer
+            services.AddDbContext<GaugeContext>(opt => opt.UseMySQL
                 (Configuration.GetConnectionString("GaugeConnection")));
 
             services.AddControllersWithViews();
