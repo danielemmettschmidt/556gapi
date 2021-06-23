d6d8c37ce84e24188abd9be385abb8a8b56a4fe1
DESKTOP-CTC3I97\Daniel <apps-daniel@emmettschmidt.com>
2021-06-23 18:26:25 -0400

    Quick backup of migration files

---
+++ GaugeSite/Migrations/20210623222253_MySQLGaugeMigration06-23-21.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GaugeSite.Migrations
+{
+    public partial class MySQLGaugeMigration062321 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMode",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMin",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMedian",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMax",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsAverage",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMode",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMin",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMedian",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMax",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceAverage",
+                table: "DateStats",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PPRMaxMerchant",
+                table: "DateStats",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PPRMinMerchant",
+                table: "DateStats",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PriceMaxMerchant",
+                table: "DateStats",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PriceMinMerchant",
+                table: "DateStats",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoundsMaxMerchant",
+                table: "DateStats",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoundsMinMerchant",
+                table: "DateStats",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PPRMaxMerchant",
+                table: "DateStats");
+
+            migrationBuilder.DropColumn(
+                name: "PPRMinMerchant",
+                table: "DateStats");
+
+            migrationBuilder.DropColumn(
+                name: "PriceMaxMerchant",
+                table: "DateStats");
+
+            migrationBuilder.DropColumn(
+                name: "PriceMinMerchant",
+                table: "DateStats");
+
+            migrationBuilder.DropColumn(
+                name: "RoundsMaxMerchant",
+                table: "DateStats");
+
+            migrationBuilder.DropColumn(
+                name: "RoundsMinMerchant",
+                table: "DateStats");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMode",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMin",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMedian",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsMax",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RoundsAverage",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMode",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMin",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMedian",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceMax",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PriceAverage",
+                table: "DateStats",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+        }
+    }
+}
--- GaugeSite/Models/DateStat.cs
+++ GaugeSite/Models/DateStat.cs
@@ -13,9 +13,15 @@ namespace Gauge.Models
 
         [Required]
         public double PPRMax { get; set; }
+
+        [Required] [MaxLength(200)]
+        public string PPRMaxMerchant { get; set; }
       
         [Required]
         public double PPRMin { get; set; }
+      
+        [Required] [MaxLength(200)]
+        public string PPRMinMerchant { get; set; }
 
         [Required]
         public double PPRAverage { get; set; }
@@ -26,7 +32,13 @@ namespace Gauge.Models
         [Required]
         public double PPRMode { get; set; }
 
+        [MaxLength(200)]
+        public string PriceMaxMerchant { get; set; }
+
         public double? PriceMax { get; set; }
+      
+        [MaxLength(200)]
+        public string PriceMinMerchant { get; set; }
       
         public double? PriceMin { get; set; }
 
@@ -37,8 +49,14 @@ namespace Gauge.Models
         public double? PriceMode { get; set; }
 
         public double? RoundsMax { get; set; }
+
+        [MaxLength(200)]
+        public string RoundsMaxMerchant { get; set; }
       
         public double? RoundsMin { get; set; }
+
+        [MaxLength(200)]
+        public string RoundsMinMerchant { get; set; }
 
         public double? RoundsAverage { get; set; }
 
